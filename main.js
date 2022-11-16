a4d44fa6b6477086f6941da282d10237ddc9ac02
unknown <minh.trancong@NSK.LOCAL>
2022-11-16 12:51:13 +0900

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+// Execute a function when the user presses a key on the keyboard
+document.getElementById("search").addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      alert("ccc")
+    }
+  });
